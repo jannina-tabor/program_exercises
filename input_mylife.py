7da0c53fbadcc8f017052f6fcae8a832c9859b8f
Jannina <jaitabor2033@gmail.com>
2024-04-25 19:08:47 +0800

    Created a Pseudocode for basis

---
+++ input_mylife.py
@@ -0,0 +1,12 @@
+# Pseudocode
+
+# Write a write function code for the 'mylife.txt' 
+# Create a while function 
+# Prompt the user to enter a line of text
+# Write the user input to the file
+# Prompt the user if there are more lines to enter by answering yes or no
+# If the user said no, close the file
+# Exit the loop of the while function
+# Create an print function to tell the user the file is closed
+
+
