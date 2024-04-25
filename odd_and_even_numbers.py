bc76f6f1afd4d856881bda37aa5006c29c694aae
Jannina <jaitabor2033@gmail.com>
2024-04-25 14:22:06 +0800

    Plan to convert the 'strings' from the number.txt into integers

--- odd_and_even_numbers.py
+++ odd_and_even_numbers.py
@@ -1,4 +1,5 @@
 # Pseudocode 
 
 # Create a text file that contains 20 integers named "numbers.txt"
-# Create a line of code that reads numbers from the tect file"
+# Create a line of code that reads numbers from the text file"
+# Convert the strings from the number.txt into integers"
