bab79ce61f2f7df8c8de3003b168cbff40c2d280
Jannina <jaitabor2033@gmail.com>
2024-04-25 17:25:39 +0800

    Created a Pseudocode for basis

---
+++ student_gwa.py
@@ -0,0 +1,9 @@
+# Psuedocode
+
+# Create a text file containing the name of 20 students with their respective GWA
+# Create a read function for the text file
+# Initialize the variables: GWA set to zero and Student to empty string ""
+# Splitting the line into name and GWA 
+# Creating an if function to determine who has the highest GWA
+# Creating an output function to determine if the program is working
+
