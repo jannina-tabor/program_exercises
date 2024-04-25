75171dc0e0f156829afaf9f969be3582f1805cee
Jannina <jaitabor2033@gmail.com>
2024-04-25 20:00:38 +0800

    Made a Pseudocode for basis of instructions

---
+++ double_even_triple_odd.py
@@ -0,0 +1,11 @@
+# Pseudocode
+
+# Create a text file consisting 20 random integers
+# Create a read function for the text file
+# Convert the strings to integers from the text file
+# Separate the odd and even numbers
+# Calculate the square of the even numbers and the cube of the odd numbers
+# Create a write function for the even squares naming 'double.txt'
+# Create a write function for the odd cubes naming 'triple.txt'
+# Make the user know that the text files have been created
+
