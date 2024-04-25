7dc20f106358ab59762bdd8981692b483149be93
Jannina <jaitabor2033@gmail.com>
2024-04-25 20:31:41 +0800

    Replaced the ',' into '.' in the write function for odd cubes

--- double_even_triple_odd.py
+++ double_even_triple_odd.py
@@ -30,7 +30,7 @@ with open ('double.txt', 'w') as even_file:
         even_file.write(str(square) + '\n')
 
 # Write odd cubes to a new text file as 'triple.txt'
-with open ('triple,txt' , 'w') as odd_file:
+with open ('triple.txt' , 'w') as odd_file:
     for cube in odd_cubes:
         odd_file.write(str(cube) + '\n')
 
