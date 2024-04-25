44e656e52268c5f4f1c897205176740cb86fea42
Jannina <jaitabor2033@gmail.com>
2024-04-25 14:27:04 +0800

    Plan to create a write function for the even numbers

--- odd_and_even_numbers.py
+++ odd_and_even_numbers.py
@@ -4,3 +4,4 @@
 # Create a line of code that reads numbers from the text file"
 # Convert the strings from the number.txt into integers"
 # Separate the odd and even numbers from the file
+# Create a 'Write to a File' code for the even numbers
